3300653eab4c3b17bacffc6f1ff8144eec5601e7
Jaeyong Cho <jaeyong.cho.ece@ajou.ac.kr>
2022-11-02 15:33:46 +0900

    Add random number generator

--- proc.c
+++ proc.c
@@ -6,6 +6,7 @@
 #include "x86.h"
 #include "proc.h"
 #include "spinlock.h"
+#define PHI 0x9e3779b9
 
 struct {
   struct spinlock lock;
@@ -24,6 +25,39 @@ void
 pinit(void)
 {
   initlock(&ptable.lock, "ptable");
+}
+
+static uint Q[4096], c = 362436;
+
+void srand(uint x)
+{
+  int i;
+
+  Q[0] = x;
+  Q[1] = x + PHI;
+  Q[2] = x + PHI + PHI;
+
+  for (i = 3; i < 4096; i++)
+    Q[i] = Q[i - 3] ^ Q[i - 2] ^ PHI ^ i;
+}
+
+uint rand(void)
+{
+  if(sizeof(unsigned long long) != 8){
+    return 0;
+  }
+  unsigned long long t, a = 18782LL;
+  static uint i = 4095;
+  uint x, r = 0xfffffffe;
+  i = (i + 1) & 4095;
+  t = a * Q[i] + c;
+  c = (t >> 32);
+  x = t + c;
+  if (x < c) {
+    x++;
+    c++;
+  }
+  return (Q[i] = r - x);
 }
 
 // Must be called with interrupts disabled
