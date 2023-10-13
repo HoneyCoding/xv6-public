f67f7a64157b9d2f562eceaf1e5672bb8988d098
HoneyCoding <dd3557@naver.com>
2023-10-13 10:49:14 +0000

    [Phase 1] update getreadcount.c with dummy code

---
+++ getreadcount.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc >= 2)
+  {
+    printf(2, "getreadcount: Too many arguments\n");
+    exit();
+  }
+
+  printf(2, "getreadcount: Test Code\n");
+
+  exit();
+}
