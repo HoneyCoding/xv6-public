edcf2bf942a8d5d532de599ec78ac78ba4a531c0
HoneyCoding <dd3557@naver.com>
2023-10-13 11:26:03 +0000

    [Phase 1] update getreadcount.c to show the result

--- getreadcount.c
+++ getreadcount.c
@@ -4,12 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+  int result;
   if (argc >= 2)
   {
     printf(2, "getreadcount: Too many arguments\n");
     exit();
   }
 
-  getreadcount();
+  if ((result = getreadcount()) == -1) {
+    printf(2, "getreadcount: Failed to read count\n");
+    exit();
+  }
+
+  printf(2, "getreadcount: read count is %d\n", result);
   exit();
 }
